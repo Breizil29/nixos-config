665cfefc976f81f51eecd966d1cf5cc68660edec
Breizil29 <81489376+Breizil29@users.noreply.github.com>
2024-08-29 14:32:03 +0200

    Update hyprland.nix

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -33,14 +33,11 @@ in
 
   services.xserver.excludePackages = [ pkgs.xterm ];
 
-  services.xserver = {
-    displayManager.sddm = {
+  services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
       enableHidpi = true;
       theme = "chili";
       package = pkgs.sddm;
     };
-
-  };
-}
+  }
